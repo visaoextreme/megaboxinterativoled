e37b62665f3b6370903346fdeed4e1baaefb2b30
visaoextreme <giftvendasnegocios@gmail.com>
2025-02-17 22:22:04 -0300

    Update backend_config.py

--- backend_config.py
+++ backend_config.py
@@ -1,21 +1,16 @@
+# backend_config.py
 import os
 
-# ----------------------------
-# Defina aqui todas as variáveis de configuração
-# ----------------------------
 AUTH_TOKEN = os.environ.get("AUTH_TOKEN", "megabox-123")
 LOG_FILE_SERVER = os.environ.get("LOG_FILE_SERVER", "server.log")
 PORT = int(os.environ.get("PORT", 5000))
 DEBUG_MODE = bool(os.environ.get("DEBUG_MODE", False))
 SECRET_API_TOKEN = os.environ.get("SECRET_API_TOKEN", "my-secret-token")
 
-# Para o front-end (kiosk) saber a URL do seu back-end:
 SIGNALING_URL = os.environ.get("SIGNALING_URL", "https://megaboxinterativoled.onrender.com")
 
-# STUN/TURN servers
 ICE_SERVERS = [{
     "urls": ["stun:stun.l.google.com:19302", "stun:global.stun.twilio.com:3478"]
 }]
 
-# Preferências de codecs de vídeo
 CODEC_PREFERENCES = ["video/H264", "video/VP8"]
